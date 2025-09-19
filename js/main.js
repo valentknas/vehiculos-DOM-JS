c68b0694e72c727fc2cc57f304b8383b87adb717
Valentina <valentnaran@gmail.com>
2025-09-19 13:17:45 -0500

    agregue todos los id a los inputs

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form-vehiculo');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+const valorFoto = document.getElementById('foto-input').value;
+
+});
